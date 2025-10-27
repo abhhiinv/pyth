86d285635feed7d460609e1e131ad8fb5d478ab6
Abhinav Sajeev <149265584+abhhiinv@users.noreply.github.com>
2025-10-27 21:18:05 +0530

    Add files via upload

---
+++ graphics/_3D_graphics/cuboid.py
@@ -0,0 +1,5 @@
+def surface_area(l,b,h):
+    return 2 *((l*b)+(b*h)+(l*h))
+
+def volume(l,b,h):
+    return l*b*h 
---
+++ graphics/_3D_graphics/sphere.py
@@ -0,0 +1,7 @@
+import math
+
+def surface_area(r):
+    return 4 * math.pi * r**2
+
+def volume(r):
+    return (4/3) * math.pi * r**3
