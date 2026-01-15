69c25f1bfe862646f35e9d477647eead53d777ff
Abhinav Sajeev <abhinavsajeev2004@gmail.com>
2026-01-15 15:55:37 +0530

    Refactor print_M function to improve output pattern

--- patterns/print_M.py
+++ patterns/print_M.py
@@ -2,8 +2,8 @@ def print_M(size=14):
     for i in range(size):
         for j in range(size):
             if j==0 or j==size-1 or\
-                (i<size and j==i)or\
-                (i<size//2 and j==size-1):
+                (j<size//2 and i==j) or\
+                (j>size//2 and i==size-1-j):
                 print('*',end='')
             else:
                 print(' ',end='')
