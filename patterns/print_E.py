b49d2b7e10da21b03b313b522798a29c9fb7cdd1
abh.n.v <abhinavsajeev2004@gmail.com>
2026-01-15 16:02:16 +0530

    E's middle part mod

--- patterns/print_E.py
+++ patterns/print_E.py
@@ -2,7 +2,9 @@ def print_E(size=9):
     for i in range(size):
         for j in range(size):
             if j==0 or\
-                (i==0 or i==size//2 or i==size-1):
+                i==0 or\
+                (i==size//2 and j<(size//2)+2) or\
+                i==size-1:
                 print('*',end='')
             else:
                 print(' ',end='')
